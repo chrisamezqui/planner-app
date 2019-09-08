9006f8796a00ec54c7abc68c25de44026b4a656d
chris <christopher.amezquita@gmail.com>
2019-09-08 15:37:56 -0700

    home page modifications

--- webServices/app/http/web/app/src/Home/index.js
+++ webServices/app/http/web/app/src/Home/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { withAuth } from '@okta/okta-react';
 
-const home = (props) => {
-  return (
-    <div>Home</div>
-  )
-};
 
-export default home;
+// const home = (props) => {
+//   return (
+//     <div>Home</div>
+//   )
+// };
+
+
+
+export default withAuth(Home);
--- webServices/app/repository/mongo_constants.py
+++ webServices/app/repository/mongo_constants.py
@@ -1,6 +1,8 @@
-# MONGO_URL = 'mongodb://mongo_user:mongo_secret@0.0.0.0:27017/'
+# Mongodb dev connection info
 MONGO_URL = 'mongodb://mongo_user:mongo_secret@192.168.99.100:27017/'
 INSTANCE_NAME = 'devdb'
+
+#Mongodb collection names
 STICKYNOTE_COLLECTION_NAME = 'stickynotes'
 POSTTYPE_COLLECTION_NAME = 'posttypes'
 COLLECTIONS = [STICKYNOTE_COLLECTION_NAME, POSTTYPE_COLLECTION_NAME]
